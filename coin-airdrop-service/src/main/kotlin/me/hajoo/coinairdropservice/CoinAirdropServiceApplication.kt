21166a1cf8ea29a5b6a128e7342ff8c5dd0e5ce7
joohyun.ha <mylifeforcoding@gmail.com>
2025-07-13 18:29:48 +0900

    init: coin-airdop-service

---
+++ coin-airdrop-service/src/main/kotlin/me/hajoo/coinairdropservice/CoinAirdropServiceApplication.kt
@@ -0,0 +1,11 @@
+package me.hajoo.coinairdropservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoinAirdropServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoinAirdropServiceApplication>(*args)
+}
---
+++ coin-airdrop-service/src/test/kotlin/me/hajoo/coinairdropservice/CoinAirdropServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.hajoo.coinairdropservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoinAirdropServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
